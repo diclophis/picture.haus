634c7977129a82c81152aeb70c81abc6c055df72
Diclophis <diclophis@gmail.com>
2013-07-24 03:25:58 -0700

    fix up some root linkage

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -5,4 +5,9 @@ describe 'the signup page' do
     visit new_person_registration_path
     page.save_screenshot("tmp/screenshots/signup_spec.png")
   end
+
+  it "should link to root" do
+    visit root_path
+    page.should have_link "centerology", root_path
+  end
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -15,4 +15,9 @@ describe 'the welcome page' do #, :js => true do #, :js => true do
     visit root_path
     page.should have_link 'Land of the Rising Code', "http://risingcode.com"
   end
+
+  it "should link to sign up" do
+    visit root_path
+    page.should have_link 'sign up', new_person_registration_path
+  end
 end
