8c6641c7d0317c7137935876881514d8adaf83ad
Diclophis <diclophis@gmail.com>
2013-07-24 21:52:26 -0700

    signin spec

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'the signin page' do
+  it "should have a nice look" do
+    visit new_person_session_path
+    page.save_screenshot("tmp/screenshots/signin_spec.png")
+  end
+
+  it "should link to root" do
+    visit root_path
+    page.should have_link "centerology", root_path
+  end
+end
