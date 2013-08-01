3721442d7922fca94f32b94ca1a083086388e331
Diclophis <diclophis@gmail.com>
2013-08-01 01:18:24 -0700

    more cleanup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,7 @@ class WelcomeController < ApplicationController
     @recent_images = Image.paginate(
       :per_page => current_per_page,
       :page => current_page, 
-      #:select => "images.*, count(*) as popularity, DATEDIFF(NOW(), findings.created_at) as newness",
       :select => "images.*, count(*) as popularity, MAX(findings.created_at) as newness",
-      #:joins => "JOIN findings ON (findings.image_id = images.id AND DATEDIFF(NOW(), findings.created_at) < 1)",
       :joins => "JOIN findings ON (findings.image_id = images.id)",
       :group => "findings.image_id",
       :order => "newness DESC, popularity DESC, findings.created_at DESC"
@@ -18,6 +16,10 @@ class WelcomeController < ApplicationController
       @friendship = Friendship.new
       @friendship.person_id = @feeder.id
       @friendship.friend_id = current_person.to_param
+      @images = @feeder.images.paginate(
+        :per_page => current_per_page,
+        :page => current_page, 
+      )
     else
       flash[:alert] = "feed not found"
       render :action => :index, :status => :not_found
