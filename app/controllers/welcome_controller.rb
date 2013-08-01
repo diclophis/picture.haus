daf76f12e9eb1b8642753d71c38813a6b10686d6
Diclophis <diclophis@gmail.com>
2013-07-31 20:47:14 -0700

    make welcome spec more awesome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    @recent_images = Image.paginate(
+      :per_page => current_per_page,
+      :page => current_page, 
+      #:select => "images.*, count(*) as popularity, DATEDIFF(NOW(), findings.created_at) as newness",
+      :select => "images.*, count(*) as popularity, MAX(findings.created_at) as newness",
+      #:joins => "JOIN findings ON (findings.image_id = images.id AND DATEDIFF(NOW(), findings.created_at) < 1)",
+      :joins => "JOIN findings ON (findings.image_id = images.id)",
+      :group => "findings.image_id",
+      :order => "newness DESC, popularity DESC, findings.created_at DESC"
+    )
   end
 
   def feed
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -20,4 +20,30 @@ describe 'the welcome page' do #, :js => true do #, :js => true do
     visit root_path
     page.should have_link 'sign up', new_person_registration_path
   end
+
+  it "should have awesome images" do
+    person = valid_person
+    person.save.should be_true
+    database = Time.now.to_i
+
+    ImageSeek.daemon {
+      ImageSeek.create(database).should == database 
+      Dir.glob(Rails.root.join("app/assets/images/*")).each do |f|
+        image = valid_image
+        image.src = "app/assets/images/" + File.basename(f)
+        image.save.should be_true
+        finding = valid_finding
+        finding.person = person
+        finding.image = image
+        finding.save.should be_true
+
+        ImageSeek.add_image(database, image.id, image.src, is_url = false).should == 1
+        #similar_without_keywords = ImageSeek.find_images_similar_to(database, image_one.id, 4).should_not be_empty
+      end
+    }
+
+    visit root_path
+    page.save_screenshot("tmp/screenshots/welcome_spec_001.png", :full => true)
+
+  end
 end
