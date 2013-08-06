f92e2ddda3ee0e09c37537326c8613f26e675b3c
Diclophis <diclophis@gmail.com>
2013-08-05 18:41:18 -0700

    hard-code path to bundle, this needs to be fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,7 @@ namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export, :roles => :app do
     run "cd #{current_path} && rbenv sudo bundle exec foreman export daemon /etc/init -a #{application} -u ubuntu -l /var/log/centerology"
+    run "for f in `ls /etc/init/#{application}*.conf`; do rbenv sudo sed -i.bak 's/ --exec bundle / --exec \\/home\\/ubuntu\\/.rbenv\\/shims\\/bundle /' $f; done;"
 
 
     #run "cd #{current_path} && rbenv sudo bundle exec foreman export upstart /etc/init -a #{application} -u ubuntu -l /var/log/centerology"
