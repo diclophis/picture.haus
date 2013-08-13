9911423a420620acfd1941bb7bc3ff4b22a3764a
Diclophis <diclophis@gmail.com>
2013-08-12 23:42:42 -0700

    more css cleanup

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -11,11 +11,6 @@ describe 'the welcome page' do #, :js => true do #, :js => true do
     page.should have_link "root", root_path
   end
 
-  it "should link to risingcode.com" do
-    visit root_path
-    page.should have_link 'Land of the Rising Code', "http://risingcode.com"
-  end
-
   it "should link to sign up" do
     visit root_path
     page.should have_link 'sign up', new_person_registration_path
