4088aef250131ac335e57169417fd82585706a33
Diclophis <diclophis@gmail.com>
2013-08-19 00:53:33 -0700

    thats horrible

--- app/models/finding.rb
+++ app/models/finding.rb
@@ -10,6 +10,16 @@ class Finding < ActiveRecord::Base
   after_save :add_image_to_image_seek
 
   def add_image_to_image_seek
+
+    $imageseek_databases = ImageSeek.databases
+
+    $imageseek_database = $imageseek_databases.first 
+
+    unless $imageseek_database
+      $imageseek_database = "index-000" # Time.now.to_i || rand ?
+      ImageSeek.create($imageseek_database)
+    end
+
     image_added = ImageSeek.add_image($imageseek_database, self.image.id, self.image.src, is_url = true)
     p image_added
 
--- config/initializers/imageseek.rb
+++ config/initializers/imageseek.rb
@@ -1,10 +1,2 @@
 require 'imageseek'
 
-$imageseek_databases = ImageSeek.databases
-
-$imageseek_database = $imageseek_databases.first 
-
-unless $imageseek_database
-  $imageseek_database = "index-000" # Time.now.to_i || rand ?
-  ImageSeek.create($imageseek_database)
-end
